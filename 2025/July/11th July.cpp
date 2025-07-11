0d60e02a6445e51bf82f5810bc5cd9bd72f9e577
Tejeswar Srinivasan <tejeswar2019@gmail.com>
2025-07-11 22:11:47 +0530

    11th July

---
+++ 2025/July/11th July.cpp
@@ -0,0 +1,29 @@
+# include<iostream>
+# include<vector>
+
+using namespace std;
+
+void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> columnZero(n, 0);
+        vector<int> rowZero(m, 0);
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    columnZero[j] = 1;
+                    rowZero[i] = 1;
+                }
+            }
+        }        
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(columnZero[j] == 1|| rowZero[i] == 1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
